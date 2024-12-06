57e1f4b90b839d27d8737f4401948919bab22682
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2024-12-05 23:23:01 -0300

    cuarto Commit

--- src/views/userRegister/userRegister.tsx
+++ src/views/userRegister/userRegister.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { validateLogin, validateRegister } from "@/helpers/validate";
+import { validateRegister } from "@/helpers/validate";
 import Swal from "sweetalert2";
 
 const UserRegister = () => {
